668a8fcf442697724dd2208045e9f8ae6aeebd59
whmountains <8680147+whmountains@users.noreply.github.com>
2017-10-19 19:20:48 -0300

    Minify HTML, enable server push

---
+++ update-headers.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env fish
+
+cd build
+
+cp -f _headers-tpl _headers
+
+sed -i '' 's#GRAY_LOGO#'(ls static/media/logo-gray*)'#' _headers
+sed -i '' 's#YOUTUBE_PLAY#'(ls static/media/youtube-play*)'#' _headers
+sed -i '' 's#SCROLL_DOWN#'(ls static/media/scroll-down*)'#' _headers
