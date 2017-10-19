733ed4ccf423c715c1b929ca301885ca9209fa44
whmountains <8680147+whmountains@users.noreply.github.com>
2017-10-19 19:27:15 -0300

    Let's try without the space :|

--- update-headers.sh
+++ update-headers.sh
@@ -6,6 +6,6 @@ cd build
 
 cp -f _headers-tpl _headers
 
-sed -i '' 's#GRAY_LOGO#'`ls static/media/logo-gray*`'#' _headers
-sed -i '' 's#YOUTUBE_PLAY#'`ls static/media/youtube-play*`'#' _headers
-sed -i '' 's#SCROLL_DOWN#'`ls static/media/scroll-down*`'#' _headers
+sed -i'' 's#GRAY_LOGO#'`ls static/media/logo-gray*`'#' _headers
+sed -i'' 's#YOUTUBE_PLAY#'`ls static/media/youtube-play*`'#' _headers
+sed -i'' 's#SCROLL_DOWN#'`ls static/media/scroll-down*`'#' _headers
